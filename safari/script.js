5c8614d85a524561716ea3c5f4b59010c2650673
Michelle Giray <mitchie028@gmail.com>
2026-06-05 23:09:31 +0800

    Update script.js

--- safari/script.js
+++ safari/script.js
@@ -1,1 +1,22 @@
+window.addEventListener("load", () => {
 
+  const whoosh1 = document.getElementById("whoosh1");
+  const whoosh2 = document.getElementById("whoosh2");
+  const roar = document.getElementById("roar");
+
+  setTimeout(() => {
+    whoosh1.volume = 0.6;
+    whoosh1.play().catch(()=>{});
+  }, 900);
+
+  setTimeout(() => {
+    whoosh2.volume = 0.5;
+    whoosh2.play().catch(()=>{});
+  }, 4500);
+
+  setTimeout(() => {
+    roar.volume = 0.7;
+    roar.play().catch(()=>{});
+  }, 6000);
+
+});
