a40321cd7865ccc25f43d613f328cccfdabb3a2c
Michelle Giray <mitchie028@gmail.com>
2026-06-06 13:09:10 +0800

    Update script.js

--- safari/script.js
+++ safari/script.js
@@ -6,38 +6,27 @@ window.addEventListener("load", () => {
   const title = document.getElementById("title");
   const rsvp = document.getElementById("rsvp");
 
-  // STEP 1: CLICK TO START
   startScreen.addEventListener("click", async () => {
 
-    // hide intro
-    startScreen.style.opacity = "0";
-    startScreen.style.pointerEvents = "none";
+    startScreen.style.display = "none";
 
-    setTimeout(() => {
-      startScreen.style.display = "none";
-    }, 800);
-
-    // STEP 2: play video
     try {
       await video.play();
     } catch (e) {
-      console.log("Video autoplay blocked");
+      console.log("Autoplay blocked");
     }
 
-    // STEP 3: Ethan appears (after 3s)
     setTimeout(() => {
       ethan.style.opacity = "1";
-    }, 3000);
+    }, 2000);
 
-    // STEP 4: Title reveal (5s)
     setTimeout(() => {
       title.style.opacity = "1";
-    }, 5000);
+    }, 3500);
 
-    // STEP 5: RSVP reveal (7s)
     setTimeout(() => {
       rsvp.style.opacity = "1";
-    }, 7000);
+    }, 5000);
 
   });
 
