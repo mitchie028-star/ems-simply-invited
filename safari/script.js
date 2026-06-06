cd237adba13b12c78237635a7a7780064a450a7e
Michelle Giray <mitchie028@gmail.com>
2026-06-06 13:23:50 +0800

    Update script.js

--- safari/script.js
+++ safari/script.js
@@ -6,32 +6,19 @@ window.addEventListener("load", () => {
   const title = document.getElementById("title");
   const rsvp = document.getElementById("rsvp");
 
-  const leafLeft = document.getElementById("leafLeft");
-  const leafRight = document.getElementById("leafRight");
-
   startScreen.addEventListener("click", async () => {
 
     startScreen.style.display = "none";
 
-    // play video
     try {
       await video.play();
     } catch (e) {}
 
-    // 🌿 LEAVES OPEN
     document.body.classList.add("open");
 
-    // 👶 ETHAN appears
-    setTimeout(() => {
-      ethan.style.opacity = "1";
-    }, 2000);
+    setTimeout(() => ethan.style.opacity = "1", 2000);
+    setTimeout(() => title.style.opacity = "1", 3200);
 
-    // 📝 TITLE
-    setTimeout(() => {
-      title.style.opacity = "1";
-    }, 3200);
-
-    // ✉️ RSVP SCENE (FULL SCREEN)
     setTimeout(() => {
       rsvp.style.opacity = "1";
       rsvp.style.pointerEvents = "auto";
