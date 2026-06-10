b82f8bb681f57719174df1cc8df96f731bcde000
Michelle Giray <mitchie028@gmail.com>
2026-06-10 13:34:34 +0800

    Update script.js

--- christening-invitation/script.js
+++ christening-invitation/script.js
@@ -1,1 +1,17 @@
+window.addEventListener("load", () => {
 
+  const intro = document.getElementById("intro");
+  const hero = document.querySelector(".hero");
+
+  // Step 1: open clouds
+  setTimeout(() => {
+    intro.classList.add("open");
+  }, 500);
+
+  // Step 2: reveal hero
+  setTimeout(() => {
+    intro.classList.add("hide");
+    hero.classList.remove("hidden");
+  }, 3200);
+
+});
