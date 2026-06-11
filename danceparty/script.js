b279235a103b99b281a2c0973aa344963971b555
Michelle Giray <mitchie028@gmail.com>
2026-06-11 12:52:29 +0800

    Update script.js

--- danceparty/script.js
+++ danceparty/script.js
@@ -1,1 +1,17 @@
+document.getElementById("subtitle").textContent =
+    invitation.subtitle;
 
+document.getElementById("name").textContent =
+    invitation.name;
+
+document.getElementById("title").textContent =
+    invitation.title;
+
+document.getElementById("date").textContent =
+    invitation.date;
+
+document.getElementById("time").textContent =
+    invitation.time;
+
+document.getElementById("venue").textContent =
+    invitation.venue;
