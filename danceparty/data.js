f5012588013f5853f5be8dcb22edd0d5706c1fbc
Michelle Giray <mitchie028@gmail.com>
2026-06-11 12:52:19 +0800

    Update data.js

--- danceparty/data.js
+++ danceparty/data.js
@@ -1,1 +1,11 @@
+const invitation = {
+    subtitle: "You're Invited",
+    name: "Sarah",
+    title: "18th Birthday Party",
 
+    date: "Saturday, June 20, 2026",
+    time: "7:00 PM",
+    venue: "ABC Events Place",
+
+    rsvp: "0917-123-4567"
+};
