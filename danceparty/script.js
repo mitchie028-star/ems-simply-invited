4b5996022e58a40939c3fd96800a8ab68fdd4c9a
Michelle Giray <mitchie028@gmail.com>
2026-06-11 16:25:53 +0800

    Update script.js

--- danceparty/script.js
+++ danceparty/script.js
@@ -3,9 +3,13 @@ window.addEventListener("load", () => {
   if (video) {
     video.muted = true;
     video.playsInline = true;
+    
+    // Attempt autoplay
     const playPromise = video.play();
     if (playPromise !== undefined) {
-      playPromise.catch(() => console.log("Autoplay blocked."));
+      playPromise.catch(() => {
+        console.log("Autoplay blocked, user interaction required.");
+      });
     }
   }
 });
