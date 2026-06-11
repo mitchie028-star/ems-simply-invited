ceb50375a7a0f4dad65c3d2f41996ef515b2984a
Michelle Giray <mitchie028@gmail.com>
2026-06-11 20:41:13 +0800

    Update script.js

--- danceparty/script.js
+++ danceparty/script.js
@@ -1,14 +1,16 @@
 window.addEventListener("load", () => {
   const music = document.getElementById("bg-music");
-  const startAudio = () => {
-    music.play().catch(e => console.warn(e));
-    document.removeEventListener('click', startAudio);
-    document.removeEventListener('touchstart', startAudio);
-  };
-  document.addEventListener('click', startAudio);
-  document.addEventListener('touchstart', startAudio);
-});
+  const video = document.querySelector(".bg-video");
 
-function toggleRSVP() {
-    document.getElementById('rsvp-section').classList.toggle('hidden');
-}
+  if (video) {
+    video.muted = true;
+    video.play().catch(e => console.log(e));
+  }
+
+  const startAudio = () => {
+    music.play().catch(e => console.log(e));
+    document.removeEventListener('click', startAudio);
+  };
+
+  document.addEventListener('click', startAudio);
+});
